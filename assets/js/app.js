c0193dfe847c2dd20b25b68074205c8ec1de3a61
Ksu <kseniapril2015@gmail.com>
2023-01-29 15:39:47 +0200

    pop_up_open

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 function openLoginWindow(){
-    let pop_up_login = document.getElementsByClassName('pop-up-login');
+    let pop_up_login = document.getElementById('pop_up_login');
     console.log(pop_up_login);
-    pop_up_login.style.display = 'block';
+    pop_up_login.style.display = 'flex';
 }
