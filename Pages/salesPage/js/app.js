ae2efe899b7479106de51e3f4a2df540786bb495
orynhydro <oreshnikovvl@gmail.com>
2023-02-02 22:23:32 +0200

    second slider added

---
+++ Pages/salesPage/js/app.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    speed: 800,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
+swiper.allowTouchMove = false;
