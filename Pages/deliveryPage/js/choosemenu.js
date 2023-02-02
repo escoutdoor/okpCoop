e43b341a9dcd9449171b502fd3ec6b3d8646a40a
escoutdoor <vanap387@gmail.com>
2023-02-02 22:14:20 +0200

    priceCounter

--- Pages/deliveryPage/js/choosemenu.js
+++ Pages/deliveryPage/js/choosemenu.js
@@ -1,26 +1,3 @@
-// let country = document.getElementById('placeholder__inst')
-// let countryMenu = document.getElementById('chooseMenu__delivery');
-
-// country.onclick = function() {
-//     countryMenu.classList.toggle('activeMenu');
-//     country.classList.toggle('activeCountry');
-//     servicesMenu.classList.remove('activeMenuServ');
-//     servicesTitle.classList.remove('activeServ');
-// }
-
-// let crItem = document.querySelectorAll('chooseMenu__delivery-item');
-
-
-// let servicesTitle = document.getElementById('placeholder__instNew')
-// let servicesMenu = document.getElementById('chooseMenu__deliveryPlus');
-
-// servicesTitle.onclick = function() {
-//     servicesMenu.classList.toggle('activeMenuServ');
-//     servicesTitle.classList.toggle('activeServ');
-//     countryMenu.classList.remove('activeMenu');
-//     country.classList.remove('activeCountry');
-// }
-
 let country = document.getElementById('placeholder__inst')
 let countryMenu = document.getElementById('chooseMenu__calc');
 
