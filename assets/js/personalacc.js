dcd103b2165de78c3908bc1ba0695f1ec5f78c31
escoutdoor <vanap387@gmail.com>
2023-02-05 20:04:44 +0200

    personalCab

--- assets/js/personalacc.js
+++ assets/js/personalacc.js
@@ -3,18 +3,28 @@ const progressItem1 = $('.persAcc__data-details__slider-nav:nth-child(1)');
 const progressItem2 = $('.persAcc__data-details__slider-nav:nth-child(2)');
 const progressItem3 = $('.persAcc__data-details__slider-nav:nth-child(3)');
 
+$('.persAcc__data-details__sliderItem:nth-child(1)').show( "slow");
 progressItem1.click(function() {
     $('.persAcc__data-details__sliderItem:nth-child(1)').show( "slow");
-    $('.persAcc__data-details__sliderItem:nth-child(2)').hide( "slow", "linear" );
-    $('.persAcc__data-details__sliderItem:nth-child(3)').hide( "slow", "linear" );
+    $('.persAcc__data-details__sliderItem:nth-child(2)').slideUp( "slow", "linear" );
+    $('.persAcc__data-details__sliderItem:nth-child(3)').slideUp( "slow", "linear" );
+    $('.persAcc__data-details__slider-nav:nth-child(1)').css({'border-bottom': '2px solid var(--color-green)', "color": "var(--color-green)"})
+    $('.persAcc__data-details__slider-nav:nth-child(2)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)"})
+    $('.persAcc__data-details__slider-nav:nth-child(3)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)"})
 })
 progressItem2.click(function() {
-    $('.persAcc__data-details__sliderItem:nth-child(2)').show( "slow", "linear" );
-    $('.persAcc__data-details__sliderItem:nth-child(1)').hide( "slow", "linear" );
+    $('.persAcc__data-details__sliderItem:nth-child(2)').slideDown( "slow", "linear" );
+    $('.persAcc__data-details__sliderItem:nth-child(1)').slideUp( "slow", "linear" );
     $('.persAcc__data-details__sliderItem:nth-child(3)').hide( "slow", "linear" );
+    $('.persAcc__data-details__slider-nav:nth-child(2)').css({'border-bottom': '2px solid var(--color-green)', "color": "var(--color-green)"})
+    $('.persAcc__data-details__slider-nav:nth-child(1)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)" })
+    $('.persAcc__data-details__slider-nav:nth-child(3)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)"})
 })
 progressItem3.click(function() {
-    $('.persAcc__data-details__sliderItem:nth-child(3)').show( "slow", "linear" );
-    $('.persAcc__data-details__sliderItem:nth-child(1)').hide( "slow", "linear" );
-    $('.persAcc__data-details__sliderItem:nth-child(2)').hide( "slow", "linear" );
+    $('.persAcc__data-details__sliderItem:nth-child(3)').slideDown( "slow", "linear" );
+    $('.persAcc__data-details__sliderItem:nth-child(1)').slideUp( "slow", "linear" );
+    $('.persAcc__data-details__sliderItem:nth-child(2)').slideUp( "slow", "linear" );
+    $('.persAcc__data-details__slider-nav:nth-child(3)').css({'border-bottom': '2px solid var(--color-green)', "color": "var(--color-green)"})
+    $('.persAcc__data-details__slider-nav:nth-child(1)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)"})
+    $('.persAcc__data-details__slider-nav:nth-child(2)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)"})
 })
