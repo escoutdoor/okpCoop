8507f520cb164a8067d395bbfb28b936acdd6734
escoutdoor <vanap387@gmail.com>
2023-02-05 20:38:21 +0200

    persAcc

--- assets/js/personalacc.js
+++ assets/js/personalacc.js
@@ -28,3 +28,22 @@ progressItem3.click(function() {
     $('.persAcc__data-details__slider-nav:nth-child(1)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)"})
     $('.persAcc__data-details__slider-nav:nth-child(2)').css({'border-bottom': '1px solid #222', "color": "var(--color-black)"})
 })
+
+
+// CLOCK
+
+function RealtimeClock() {
+    let clock = new Date();
+
+    let h = clock.getHours()-2;
+    let m = clock.getMinutes();
+    let s = clock.getSeconds();
+
+    h = ("0" + h).slice(-2);
+    m = ("0" + m).slice(-2);
+    s = ("0" + s).slice(-2);
+
+    $('.realClock').html(h + " : " + m + " : " + s);
+}
+
+setInterval(() => RealtimeClock(), 1000)
